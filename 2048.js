7125b47440260a92487f314184253d3abb8d2204
LittleDouBi <liushudong55@163.com>
2014-10-26 16:39:23 +0800

    基础功能都完成了

--- 2048.js
+++ 2048.js
@@ -1,9 +1,7 @@
 //二维数组：记录棋盘上的数据
 var dataArr = new Array();
-//每个位置是否发生了碰撞
-var hasConflicted = new Array();
 //分数
-var sroce = 0;
+var score = 0;
 
 $(document).ready(function(){
     newgame();
@@ -29,10 +27,8 @@ function init(){
 	//初始化数组
 	for (var i = 0; i < 4; i++) {
 		dataArr[i] = new Array();
-		hasConflicted[i] = new Array();
 		for(var j = 0; j < 4; j++){
 			dataArr[i][j] = 0;
-			hasConflicted[i][j] = false;
 		}
 	}
 	
@@ -46,6 +42,7 @@ function getPosLeft(x,y){
 	return 20+120*y;
 }
 function updateView(){
+	$(".number-cell").remove();
 	var numberCell = "",panel = $("#panel");
 	for (var i = 0; i < 4; i++) {
 		for (var j = 0; j < 4; j++) {
@@ -65,7 +62,6 @@ function updateView(){
                 numberCell.css('color',getNumberColor( dataArr[i][j] ) );
                 numberCell.text( getNumberText( dataArr[i][j] ) );
 			}
-			hasConflicted[i][j] = false;
 		}
 	}
 }
@@ -131,14 +127,13 @@ function randomPosition(){
 				//记录该位置
 				position.x = i;
 				position.y = j;
-				//positionArr.push({x:i,y:j});
-				(function(obj){
-					positionArr.push(obj);
-				})(position);
+				positionArr.push({x:i,y:j});
+				// (function(obj){
+				// 	positionArr.push(obj);
+				// })(position);
 			}
 		}
 	}
-	console.log(positionArr);
 	var length = positionArr.length;
 	if(length == 0){
 		return false;
@@ -161,4 +156,223 @@ function showNumberWithAnimation( i , j , number){
         top:getPosTop( i , j ),
         left:getPosLeft( i , j )
     },50);
+}
+
+$(document).keydown(function(event){
+	switch(event.keyCode){
+		case 37: //left
+			var flag = moveLeft();
+            if(flag == 0){
+            	gameOver();
+            }else if(flag == 1){
+            	generateNumber();
+            }
+            break;
+        case 38: //up
+            var flag = moveTop();
+            if(flag == 0){
+            	gameOver();
+            }else if(flag == 1){
+            	generateNumber();
+            }
+            break;
+        case 39: //right
+            var flag = moveRight();
+            if(flag == 0){
+            	gameOver();
+            }else if(flag == 1){
+            	generateNumber();
+            }
+            break;
+        case 40: //down
+            var flag = moveBottom();
+            if(flag == 0){
+            	gameOver();
+            }else if(flag == 1){
+            	generateNumber();
+            }
+            break;
+        default: //default
+            break;
+	}
+});
+//可以移动返回1，格子都满了返回0，格子未满但无法移动（数字都在左边）返回2
+function moveLeft(){
+	var canMoveFlag = false, 
+	fullflag = true,//是否格子全满
+	p;//记录碰撞位置
+	for(var i = 0; i < 4; i++){
+		p = 0;
+		for(var j = 1; j < 4; j++){
+			//可以移动的条件：前一个位置是0或者前一个位置跟当showMoveAnimation();前位置数字相等
+			if(dataArr[i][j] != 0){
+				for(var k = p; k < j; k++){
+					if(dataArr[i][k] == 0){
+						showMoveAnimation(i,j,i,k);
+						dataArr[i][k] = dataArr[i][j];
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+						//位置是0,p不用改变
+					}else if(dataArr[i][k] == dataArr[i][j]){
+						showMoveAnimation(i,j,i,k);
+						p += 1;
+						dataArr[i][k] *= 2;
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+						score += dataArr[i][k];
+						updateScore(score);
+					}else{
+						p += 1;
+					}
+				}
+			}else{
+				fullflag = false;//有空格子
+			}
+		}
+	}
+	if(canMoveFlag){
+		setTimeout("updateView()",100);
+		return 1;
+	}
+	if(fullflag){
+		return 0;
+	}
+	return 2;
+}
+function moveRight(){
+	var canMoveFlag = false, 
+	fullflag = true,//是否格子全满
+	p;//记录碰撞位置
+	for(var i = 0; i < 4; i++){
+		p = 3;
+		for(var j = 2; j >= 0; j--){
+			//可以移动的条件：右边位置是0或者右边位置跟当前位置数字相等
+			if(dataArr[i][j] != 0){
+				for(k = p; k > j; k--){
+					if(dataArr[i][k] == 0){
+						showMoveAnimation(i,j,i,k);
+						dataArr[i][k] = dataArr[i][j];
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+					}else if(dataArr[i][k] == dataArr[i][j]){
+						showMoveAnimation(i,j,i,k);
+						p -= 1;
+						dataArr[i][k] *= 2; 
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+						score += dataArr[i][k];
+						updateScore(score);
+					}else{
+						p -= 1;
+					}
+				}
+			}else{
+				fullflag = false;//有空格子
+			}
+		}
+	}
+	if(canMoveFlag){
+		setTimeout("updateView()",100);
+		return 1;
+	}
+	if(fullflag){
+		return 0;
+	}
+	return 2;
+}
+function moveTop(){
+	var canMoveFlag = false, 
+	fullflag = true,//是否格子全满
+	p;//记录碰撞位置
+	for(var j = 0; j < 4; j++){
+		p = 0;
+		for(var i = 1; i < 4; i++){
+			//可以移动的条件：前一个位置是0或者前一个位置跟当前位置数字相等
+			if(dataArr[i][j] != 0){
+				for(var k = p; k < i; k++){
+					if(dataArr[k][j] == 0){
+						showMoveAnimation(i,j,k,j);
+						dataArr[k][j] = dataArr[i][j];
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+						//位置是0,p不用改变
+					}else if(dataArr[k][j] == dataArr[i][j]){
+						showMoveAnimation(i,j,k,j);
+						p += 1;
+						dataArr[k][j] *= 2;
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+						score += dataArr[k][j];
+						updateScore(score);
+					}else{
+						p += 1;
+					}
+				}
+			}else{
+				fullflag = false;//有空格子
+			}
+		}
+	}
+	if(canMoveFlag){
+		setTimeout("updateView()",100);
+		return 1;
+	}
+	if(fullflag){
+		return 0;
+	}
+	return 2;
+}
+function moveBottom(){
+	var canMoveFlag = false, 
+	fullflag = true,//是否格子全满
+	p;//记录碰撞位置
+	for(var j = 0; j < 4; j++){
+		p = 3;
+		for(var i = 2; i >= 0; i--){
+			//可以移动的条件：右边位置是0或者右边位置跟当前位置数字相等
+			if(dataArr[i][j] != 0){
+				for(k = p; k > i; k--){
+					if(dataArr[k][j] == 0){
+						showMoveAnimation(i,j,k,j);
+						dataArr[k][j] = dataArr[i][j];
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+					}else if(dataArr[k][j] == dataArr[i][j]){
+						showMoveAnimation(i,j,k,j);
+						p -= 1;
+						dataArr[k][j] *= 2; 
+						dataArr[i][j] = 0;
+						canMoveFlag = true;
+						score += dataArr[k][j];
+						updateScore(score);
+					}else{
+						p -= 1;
+					}
+				}
+			}else{
+				fullflag = false;//有空格子
+			}
+		}
+	}
+	if(canMoveFlag){
+		setTimeout("updateView()",100);
+		return 1;
+	}
+	if(fullflag){
+		return 0;
+	}
+	return 2;
+}
+function showMoveAnimation( fromx , fromy , tox, toy ){
+    var numberCell = $('#number-cell-' + fromx + '-' + fromy );
+    numberCell.animate({
+        top:getPosTop( tox , toy ),
+        left:getPosLeft( tox , toy )
+    },100);
+}
+function updateScore(score){
+	$('#score').text(score);
+}
+function gameOver(){
+	alert("GAMEOVER");
 }
